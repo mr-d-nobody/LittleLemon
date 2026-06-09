98685d6b38065895ac6e0d5b44d81c44e64b0a5b
mr-d-nobody <mr-d-nobody@users.noreply.github.com>
2026-06-09 22:16:34 +0530

    set up menu api and table booking api

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('hello/', views.sayHello, name='hello'),
-    
+    path('menu/', views.MenuItemView.as_view(), name='menu-items'),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,10 +1,31 @@
+from .models import Booking, Menu
 from django.http import HttpResponse
 from django.shortcuts import render
+
+from rest_framework import generics, viewsets
+
+from restaurant.serializers import BookingSerializer, MenuSerializer
 
 
 # Create your views here.
 def sayHello(request):
-    return HttpResponse("Hello, welcome to Little Lemon!")  
+    return HttpResponse("Hello, welcome to Little Lemon!")
+
 
 def index(request):
-    return render(request, 'index.html', {}) 
+    return render(request, 'index.html', {})
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
