3b424b62d6fd19714037fd0f9a22439472370ae2
mr-d-nobody <mr-d-nobody@users.noreply.github.com>
2026-06-09 17:29:19 +0530

    Added mysql

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,8 +1,6 @@
 
-from django import views
-from django.contrib import admin
 from django.urls import path
-from .views import index, sayHello
+from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
